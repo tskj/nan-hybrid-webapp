a85e56b60b8be4097bb71b9d29bfc2c2af6df26c
tarjei <tarjei@skole.pc>
2017-03-13 18:04:11 +0100

    Add empty Android Studio project

---
+++ app/src/main/java/no/usn/student/nan_hybrid_webapp/Addressbook.java
@@ -0,0 +1,12 @@
+package no.usn.student.nan_hybrid_webapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Addressbook extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
