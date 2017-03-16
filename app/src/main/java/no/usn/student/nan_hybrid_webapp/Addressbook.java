61a92a9111a76d129536b098fde86fa8a01d3100
tarjei <tarjei@skole.pc>
2017-03-16 18:39:32 +0100

    Lock screen orientation to portrait

--- app/src/main/java/no/usn/student/nan_hybrid_webapp/Addressbook.java
+++ app/src/main/java/no/usn/student/nan_hybrid_webapp/Addressbook.java
@@ -1,6 +1,7 @@
 package no.usn.student.nan_hybrid_webapp;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.webkit.WebView;
 
@@ -8,7 +9,9 @@ public class Addressbook extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         WebView view = new WebView(this);
         setContentView(view);
